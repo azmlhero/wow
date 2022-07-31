f320891c4f5e890eabfb772b59d1a0218fdd4cb2
azmlhero <azmlhero1@gmail.com>
2022-07-31 18:28:09 +0500

    I am changing the products list layout

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,52 +2,57 @@ import React from "react";
 import GetImage from "./GetImage";
 
 const data = [
-  { id: 0, name: "AKG Earphones", price: "300", image: "./assets/AKG.jpg", imageId: '1' },
+  {
+    id: 0,
+    name: "AKG Earphones",
+    price: "300",
+    image: "./assets/AKG.jpg",
+    imageId: "1",
+  },
   {
     id: 1,
     name: "Beats Bluetooth Earphone ",
     price: "3000",
     image: "./assets/beatsBt.jpg",
-    imageId: '2'
+    imageId: "2",
   },
   {
     id: 2,
     name: "Gionee Earphone",
     price: "400",
     image: "./assets/Gionee.jpg",
-    imageId: '3'
+    imageId: "3",
   },
   {
     id: 3,
     name: "Ligtning Cable",
     price: "500",
     image: "./assets/lightningCable.jpg",
-    imageId: '4'
+    imageId: "4",
   },
 ];
 
 export default function Body() {
-  const dataItems = data.map((product) => (
-    <div className="grid grid-flow-row grid-cols-4 gap-10">
-    <li  className="border-solid border-2 p-10 m-8 " key={product.id}>
-      <img src= {product.image} alt={product.name} />
-      <p>
-        <b className="text-2xl">{product.name}</b>
-        <i className="text-2xl">  Price :{product.price}</i>
-      <button className="bg-red-500 text-white text p-2 m-6 ">Buy Now</button>
-      </p>
 
-    </li>   
-    </div>
-  ));
+
 
   return (
     <div>
       <div className="p-10 ">
-
-          <h1 className="text-3xl font-bold">Products</h1>
-          <div >
-          <ul >{dataItems}</ul>
+        <h1 className="text-3xl font-bold">Products</h1>
+        <div>
+          <div className="grid grid-flow-row grid-cols-4 gap-10">{data.map((product) => (
+    
+    <div className="border-solid border-2 p-10 m-8 " key={product.id}>
+      <img src={product.image} alt={product.name} />
+      <p>
+        <b className="text-2xl">{product.name}</b>
+        <i className="text-2xl"> Price :{product.price}</i>
+        <button className="bg-red-500 text-white text p-2 m-6 ">
+          Buy Now
+        </button>
+      </p>
+    </div> ))  }</div>
         </div>
       </div>
     </div>
